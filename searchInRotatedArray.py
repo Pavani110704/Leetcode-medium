e3b2e03094985228ee0e9fbc7218313d4bdcf0aa
Pavani110704 <168561451+Pavani110704@users.noreply.github.com>
2025-06-25 21:30:51 +0530

    Create searchInRotatedArray.py

---
+++ searchInRotatedArray.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def search(self, nums, target):
+        if target not in nums:
+            return -1
+        
+        for i in range(len(nums)):
+            if nums[i]==target:
+                return i
